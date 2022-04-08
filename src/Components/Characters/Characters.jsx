3402abc974306999c3ab3f53dfeee2b7e33baf3a
diegosales30 <diegofelipesales23@gmail.com>
2022-04-08 14:45:39 -0300

    att

--- src/Components/Characters/Characters.jsx
+++ src/Components/Characters/Characters.jsx
@@ -13,29 +13,25 @@ const Characters = ({
   return (
     <div>
       <ul className="container-character-list">
-        {output && output.length > 0 ? (
-          output.map((current, index) => (
-            <li key={index} className="character-list">
-              <figure>
-                <img
-                  className="img-character"
-                  src={
-                    current.image === ""
-                      ? "https://geekfanaticos.fbitsstatic.net/img/p/funko-pop-harry-potter-122-harry-potter-71768/258254-1.jpg?w=540&h=540&v=no-change&qs=ignore"
-                      : current.image
-                  }
-                  alt={current.name}
-                />
-              </figure>
-              <p className="name-character">{current.name}</p>
-              <p className="home-character">
-                {current.house === "" ? "desconhecido" : current.house}
-              </p>
-            </li>
-          ))
-        ) : (
-          <h5>carregando...</h5>
-        )}
+        {output.map((current, index) => (
+          <li key={index} className="character-list">
+            <figure>
+              <img
+                className="img-character"
+                src={
+                  current.image === ""
+                    ? "https://geekfanaticos.fbitsstatic.net/img/p/funko-pop-harry-potter-122-harry-potter-71768/258254-1.jpg?w=540&h=540&v=no-change&qs=ignore"
+                    : current.image
+                }
+                alt={current.name}
+              />
+            </figure>
+            <p className="name-character">{current.name}</p>
+            <p className="home-character">
+              {current.house === "" ? "desconhecido" : current.house}
+            </p>
+          </li>
+        ))}
       </ul>
       <div>
         <button onClick={renderCharacter} className="btn-character">
